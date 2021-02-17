ebc3322b8abf796121752b62f9f6e798bed9ead7
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-02-17 23:55:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #has_many :events
+  #has_many :joins
+
+  with_option presence: true do
+    validates :name
+    validates :email, format: { with: /\A.+?@.+\z/ }
+    validates :password, confirmation: true, length: { minimum: 8 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+    validates :password_confirmation
+    validates :tel, format: { with: /\A\d{11}\z/ }
+  end
 end
