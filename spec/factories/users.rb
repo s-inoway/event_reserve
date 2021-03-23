c01eeb6824804840adcb4bf5f453e89c81dfb170
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-03-23 23:44:50 +0900

    rubocopの実施

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :user do
     name                  { Faker::Name.name }
     email                 { Faker::Internet.email }
-    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password              { "1a#{Faker::Internet.password(min_length: 6)}" }
     password_confirmation { password }
     tel                   { Faker::Number.leading_zero_number(digits: 11) }
     comment               { Faker::Lorem.sentence }
