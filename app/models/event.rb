7a1bcda43d4929dd29035b1068a56fc91ac1390d
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-04-15 22:41:55 +0900

    モデルにバリデーションを追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
+  belongs_to :user
+
+  with_options presence: true do
+    validates :title
+    validates :comment
+    validates :category_id, numericality: { other_than: 1 }
+    validates :event_datetime
+    validates :meeting_time
+    validates :place_id, numericality: { other_than: 1 }
+    validates :bring
+    validates :deadline_datetime
+  end
 end
