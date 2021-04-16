f4f57a4fb66dc6f7dc585b1721a5e8f75b77d24a
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-04-16 15:35:56 +0900

    コントローラーにcreateアクションを追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,22 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.valid?
+      @event.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :title, :image, :category_id, :comment, :event_datetime, :meeting_time, :place_id, :price, :bring, :deadline_datetime, :capacity
+    ).merge(user_id: current_user.id)
+  end
 end
