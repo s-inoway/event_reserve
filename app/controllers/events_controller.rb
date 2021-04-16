fbdc30036dd1c9a7dcb8f8f64a4aad8cffa3d52a
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-04-16 14:14:30 +0900

    「このページについて」aboutのページを作成

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
-# frozen_string_literal: true
-
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   def new
     @event = Event.new
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'abouts/index'
+  get 'about/index'
   get 'users/show'
+  get 'about', to: 'abouts#index'
   devise_for :users
   root to: 'events#index'
   resources :users, only: [:show]
-  resources :events, only: %i[index new]
+  resources :events, only: [:index, :new]
+  resources :abouts, only: [:index]
+
 end
