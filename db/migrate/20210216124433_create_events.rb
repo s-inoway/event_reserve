b5c2f4f5fda80973ec43af1bb94556a824491c26
s-inoway <s.inoway.0804.kitchey@gmail.com>
2021-07-09 19:40:56 +0900

    マイグレーションファイルのダブりを修正

--- db/migrate/20210216124433_create_events.rb
+++
@@ -1,5 +0,0 @@
-class CreateEvents < ActiveRecord::Migration[6.0]
-  def change
-    create_table :events, &:timestamps
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -52,9 +50,9 @@ ActiveRecord::Schema.define(version: 2021_04_16_062330) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
-  create_table 'joins', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8', force: :cascade do |t|
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "joins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
